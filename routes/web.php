f92f5e06b7318d9ee5fc5ef4e53c70a0e07e7ce2
Alexandre Pereira <pereira.aleandre@gmail.com>
2025-06-20 02:02:41 -0300

    Atualizacao

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->get('/', function () {
     return view('site');
@@ -23,9 +24,16 @@ Route::middleware(['auth'])->get('/inscricao', function () {
     return view('site');
 })->name('site');
 
-
-Route::get('/teste', function () {
-    return view('teste');
+Route::get('/test-mail', function () {
+    try {
+        Mail::raw('Teste de email', function ($message) {
+            $message->to('pereira.alexandre@gmail.com')
+                   ->subject('Teste Laravel Umbler');
+        });
+        return 'Email enviado com sucesso!';
+    } catch (Exception $e) {
+        return 'Erro: ' . $e->getMessage();
+    }
 });
 
 require __DIR__.'/auth.php';
