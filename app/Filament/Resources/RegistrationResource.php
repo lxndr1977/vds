326eb59c14019c8b5a2da3932453a5e381650d6c
Alexandre Pereira <pereira.aleandre@gmail.com>
2025-09-18 16:00:09 -0300

    Adiciona dados da escola no formulário de inscrição do admin

--- app/Filament/Resources/RegistrationResource.php
+++ app/Filament/Resources/RegistrationResource.php
@@ -159,7 +159,6 @@ class RegistrationResource extends Resource
    public static function getRelations(): array
    {
       return [
-         SchoolRelationManager::class,
          MembersRelationManager::class,
          ChoreographersRelationManager::class,
          DancersRelationManager::class,
